d55c3afd1cc16df0cc18234db356ba02e477caae
Inderjeet kaur <inderjeetkaur2600@gmail.com>
2020-01-09 16:47:36 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<html lang="en">
+	<head>
+		
+		<meta charset="utf-8">
+		<title>lab1</title>
+		<meta name="description" content="MAP202Lab1">
+		<meta name="author" content="inderjeet kaur">
+	</head>
+	<body>
+		
+	</body>
+<?php 
+	echo "<b>inderjeet kaur</b> </br>"; 
+	echo"<body class=\"page_bg\">";
+	echo "hello, today is ";
+	echo date('l, F jS, y'); 
+	echo "</html>";
+	
+?>
+</body>
+	</html>
